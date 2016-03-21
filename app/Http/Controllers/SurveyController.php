180919e1c71a7d24f6cde676ba53f8ec50b749f7
mikelovely <mike-lovely@hotmail.co.uk>
2016-03-21 16:45:30 +0000

    create survey controller and describe route actions

---
+++ app/Http/Controllers/SurveyController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\Survey;
+
+class SurveyController extends Controller
+{
+    public function getStart()
+    {
+
+    }
+
+    public function postStart()
+    {
+
+    }
+
+    public function getGroup()
+    {
+
+    }
+
+    public function postGroup()
+    {
+
+    }
+
+    public function getComplete()
+    {
+
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,3 +3,29 @@
 Route::get('/', [
     'uses' => 'TestController@test'
 ]);
+
+Route::get('/[survey]/welcome', [
+    'uses' => 'SurveyController@getStart'
+]);
+
+Route::post('/[survey]/welcome', [
+    'uses' => 'SurveyController@postStart'
+]);
+
+Route::get('/[survey]/[group]', [
+    'uses' => 'SurveyController@getGroup'
+]);
+
+Route::post('/[survey]/[group]', [
+    'uses' => 'SurveyController@postGroup'
+]);
+
+Route::get('/[survey]/complete', [
+    'uses' => 'SurveyController@getComplete'
+]);
+
+Route::get('/admin', [
+    'as' =>'admin.index',
+    'uses' => 'AdminController@index',
+    'middleware' => ['auth'],
+]);
